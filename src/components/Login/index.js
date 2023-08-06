017f5fd93a83968727fe6306650ec42cdcebcde4
Akrishnaveni <125814322+Akrishnaveni@users.noreply.github.com>
2023-08-06 23:25:09 +0530

    Update index.js

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,9 +1,12 @@
 import "./index.css"
 
 class Login{
+    render(){
     return (
-        <h1 className="welcome" onclick = {this.Onclick}>Welcome User</h1>
+        <h1 className="welcome">Welcome User</h1>
         <button className="btn">Logout</button>
     )
 }
+}
+
 export default Login
