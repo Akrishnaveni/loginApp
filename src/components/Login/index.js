b942e7ef47667324507dcac0bd957d948742b49b
Akrishnaveni <125814322+Akrishnaveni@users.noreply.github.com>
2023-08-07 18:40:11 +0530

    Update index.js

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,8 +3,10 @@ import "./index.css"
 class Login{
     render(){
     return (
+        <div>
         <h1 className="welcome">Welcome User</h1>
         <button className="btn">Logout</button>
+        </div>
     )
 }
 }
