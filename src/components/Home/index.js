40ea3ddbf1d26b9a9d3d5da3aa631284a10fb666
Akrishnaveni <125814322+Akrishnaveni@users.noreply.github.com>
2023-08-07 19:05:51 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,9 +5,11 @@ import Logout from '../Logout/index.js'
 
 class Home extends Component {
   state = {islogin: false}
+  
   Onclick = () => {
     this.setState(prevState => ({islogin: !prevState.islogin}))
   }
+  
   render() {
     const {islogin} = this.state
     return (
