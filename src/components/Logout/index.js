c31f037a03c8589c8d3033ff649c9b82cc680cec
Akrishnaveni <125814322+Akrishnaveni@users.noreply.github.com>
2023-08-12 11:39:40 +0530

    Update index.js

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -1,12 +1,16 @@
-import "./index.css"
+import './index.css'
 
-const Logout=()=>(
-        <div>
-          <h1 className="please">Please Login</h1>
-          <button className="btn">Login</button>
-        </div>
-    )
+const Logout = props => {
+  const {onclick} = props
 
-
+  return (
+    <div className="container">
+      <h1 className="please">Please Login</h1>
+      <button type="button" className="btn" onClick={onclick}>
+        Login
+      </button>
+    </div>
+  )
+}
 
 export default Logout
