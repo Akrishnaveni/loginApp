544b4b2906eff79189a770acc3e4f412c0a54bc3
Akrishnaveni <125814322+Akrishnaveni@users.noreply.github.com>
2023-08-12 11:36:58 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,26 +1,23 @@
-
-import Login from '../Login/index'
-import Logout from '../Logout/index'
 import './index.css'
 import {Component} from 'react'
-
+import Login from '../Login/index'
+import Logout from '../Logout/index'
 
 class Home extends Component {
   state = {islogin: false}
-  
+
   Onclick = () => {
     this.setState(prevState => ({islogin: !prevState.islogin}))
   }
-  
+
   render() {
     const {islogin} = this.state
     return (
       <div className="container">
-        {islogin && <Login/>}
-        {!islogin && <Logout/>}
+        {islogin && <Login onclick={this.Onclick} />}
+        {!islogin && <Logout onclick={this.onclick} />}
       </div>
     )
   }
 }
-
 export default Home
